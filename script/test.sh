bd9013008f67fd3536966e121b31d8a2913d85c9
Donovan Adams <donovan@hydrotik.com>
2015-12-08 00:36:59 -0500

    remove coverage

--- script/test.sh
+++ script/test.sh
@@ -7,7 +7,7 @@ if [[ ! -d "$IMPORT_PATH" ]]; then IMPORT_PATH="$PWD"; fi
 source "$IMPORT_PATH/helpers.sh"
 
 # Remove previously generated artifacts.
-#run "rimraf $COVERAGE_OUTPUT"
+run "rimraf $COVERAGE_OUTPUT"
 
 # Monitor source files and run unit tests when files are modified.
 run "env NODE_ENV=test karma start karma.conf.dev.js" &
