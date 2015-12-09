740b6ea76c409e773c4e36c68127cfa8a7df54da
Donovan Adams <donovan@hydrotik.com>
2015-12-09 15:27:02 -0500

    proper check nom version

--- script/setup.sh
+++ script/setup.sh
@@ -6,21 +6,96 @@ IMPORT_PATH="${BASH_SOURCE%/*}"
 if [[ ! -d "$IMPORT_PATH" ]]; then IMPORT_PATH="$PWD"; fi
 source "$IMPORT_PATH/helpers.sh"
 
-NODE_VERSION=4.2.0
-NPM_VERSION=3.3.0
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        echo "equal"
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            echo "greater"
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            echo "less than"
+            return 2
+        fi
+    done
+    return 0
+}
+
+# display a message in red with a cross by it
+# example
+# echo echo_fail "No"
+function echo_fail {
+  # echo first argument in red
+  printf "\e[31m✘ ${1}"
+  # reset colours back to normal
+  echo "\033[0m"
+}
+
+# display a message in green with a tick by it
+# example
+# echo echo_fail "Yes"
+function echo_pass {
+  # echo first argument in green
+  printf "\e[32m✔ ${1}"
+  # reset colours back to normal
+  echo "\033[0m"
+}
+
+# echo pass or fail
+# example
+# echo echo_if 1 "Passed"
+# echo echo_if 0 "Failed"
+function echo_if {
+  if [ $1 == 1 ]; then
+    echo_pass $2
+  else
+    echo_fail $2
+  fi
+}
+
+TARGET_NODE_VERSION=4.2.0
+TARGET_NPM_VERSION=3.3.0
+
+NODE_VERSION="$(node --version | sed 's/[^0-9.]*//g')"
+NPM_VERSION="$(npm --version | sed 's/[^0-9.]*//g')"
 
 # Save script's current directory
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #cd "${DIR}"
 
+echo "node:     ${NODE_VERSION} $(echo_if 1)"
+echo "npm:     ${NPM_VERSION} $(echo_if 1)"
+
+
+
+echo "node: $(vercomp ${NODE_VERSION} ${TARGET_NODE_VERSION})"
 
 #
 # Check if Node is installed and at the right version
 #
-echo "Checking for Node version ${NODE_VERSION}"
-if [[ $(node --version | awk '{print $2}') > ${NODE_VERSION} ]]; then
+echo "Checking for Node version ${TARGET_NODE_VERSION}"
+if [[ "$(vercomp ${NODE_VERSION} ${TARGET_NODE_VERSION})" == "2" ]]; then
     echo "Node version does NOT meet requirements"
-    echo "Please install nvm and use node ${NODE_VERSION} or greater"
+    echo "Please install nvm and use node ${TARGET_NODE_VERSION} or greater"
     exit 0
 else
     echo "Node version meets requirements"
@@ -30,13 +105,14 @@ fi
 # Check if Node Package Manager is installed and at the right version
 #
 echo "Checking for NPM version ${NPM_VERION}"
-if [[ $(npm --version | awk '{print $2}') > ${NPM_VERSION} ]]; then
+if [[ "$(vercomp ${NPM_VERSION} ${TARGET_NPM_VERSION})" == "2" ]]; then
     echo "NPM version does NOT meet requirements"
-    echo "Please install nvm and use npm ${NPM_VERSION} or greater"
+    echo "Please install nvm and use npm ${TARGET_NPM_VERSION} or greater"
     exit 0
 else
     echo "NPM version meets requirements"
 fi
+
 
 run 'npm config set registry http://registry.npmjs.org/'
 
@@ -54,7 +130,7 @@ else
 fi
 
 # Install Karma globally if not already installed.
-if [ ! -x "$(command -v karma-cli)" ]; then
+if [ ! -x "$(command -v karma)" ]; then
     echo "WARNING: karma-cli command not found, installing globally."
     run "sudo npm install -g karma-cli" &
     KARMA_PID=$!
