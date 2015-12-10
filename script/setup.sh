dff3c94c16c50d94cb2eb2477423b2034438155b
Donovan Adams <donovan@hydrotik.com>
2015-12-10 10:19:15 -0500

    setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -10,26 +10,28 @@ source "$IMPORT_PATH/helpers.sh"
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #cd "${DIR}"
 
+
+#########################################################################################
+# Software version targets
+TARGET_NODE_VERSION=4.2.0
+TARGET_NPM_VERSION=3.3.0
+
+
+
+
+#########################################################################################
 ## each separate version number must be less than 3 digit wide !
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
-# display a message in red with a cross by it
-# example
-# echo echo_fail "No"
+# Print ✘
 function echo_fail {
-  # echo first argument in red
   printf "\e[31m✘ ${1}"
-  # reset colours back to normal
   echo "\033[0m"
 }
 
-# display a message in green with a tick by it
-# example
-# echo echo_fail "Yes"
+# Print ✔
 function echo_pass {
-  # echo first argument in green
   printf "\e[32m✔ ${1}"
-  # reset colours back to normal
   echo "\033[0m"
 }
 
@@ -45,99 +47,148 @@ function echo_if {
   fi
 }
 
-TARGET_NODE_VERSION=4.2.0
-TARGET_NPM_VERSION=3.3.0
+# Terminal output formatting
+function echo_cause {
+    printf "\e[1;37m${1}"
+}
 
+function echo_effect {
+    printf "\e[32m${1}"
+}
+
+function echo_warn {
+    printf "\e[1;33m${1}"
+}
+
+function echo_success {
+    printf "\e[1;32m${1}"
+}
+
+function echo_clear {
+    echo "\033[0m${1}"
+}
+
+
+#########################################################################################
+# Capture versions
 NODE_VERSION="$(node --version | sed 's/[^0-9.]*//g')"
 NPM_VERSION="$(npm --version | sed 's/[^0-9.]*//g')"
 
+
+
+#########################################################################################
 #
 # Check if Node is installed and at the right version
 #
-echo "Checking for Node version ${TARGET_NODE_VERSION} or greater"
+echo "$(echo_cause)Checking for Node version ${TARGET_NODE_VERSION} or greater$(echo_clear)"
 if [ "$(version "$TARGET_NODE_VERSION")" -gt "$(version "$NODE_VERSION")" ]; then
-    echo "Node version does NOT meet requirements $(echo_if 0)"
-    echo "Please install nvm and use node ${TARGET_NODE_VERSION} or greater"
+    echo "$(echo_warn)Node version does NOT meet requirements $(echo_if 0)$(echo_clear)"
+    echo "Please install nvm and use node ${TARGET_NODE_VERSION} or greater$(echo_clear)"
     exit 0
 else
-    echo "Node version ${NODE_VERSION} meets requirements $(echo_if 1)"
+    echo "$(echo_effect)Node version ${NODE_VERSION} meets requirements $(echo_if 1)$(echo_clear)"
 fi
 
 #
-# Check if Node Package Manager is installed and at the right version
+# Check if NPM is installed and at the right version
 #
-echo "Checking for NPM version ${TARGET_NPM_VERSION} or greater"
+echo "$(echo_cause)Checking for NPM version ${TARGET_NPM_VERSION} or greater$(echo_clear)"
 if [ "$(version "$TARGET_NPM_VERSION")" -gt "$(version "$NPM_VERSION")" ]; then
-    echo "Node version does NOT meet requirements $(echo_if 0)"
-    echo "Please install nvm and use node ${TARGET_NODE_VERSION} or greater"
+    echo "$(echo_warn)Node version does NOT meet requirements $(echo_if 0)$(echo_clear)"
+    echo "Please install nvm and use node ${TARGET_NODE_VERSION} or greater$(echo_clear)"
     exit 0
 else
-    echo "NPM version ${NPM_VERSION} meets requirements $(echo_if 1)"
+    echo "$(echo_effect)NPM version ${NPM_VERSION} meets requirements $(echo_if 1)$(echo_clear)"
 fi
-
-run 'npm config set registry http://registry.npmjs.org/'
-
-# Clean dependencies
-run "npm prune"
 
 # Install Nodemon globally if not already installed.
+echo "$(echo_cause)Checking for Nodemon$(echo_clear)"
 if [ ! -x "$(command -v nodemon)" ]; then
-    echo "WARNING: nodemon command not found, installing globally. $(echo_if 0)"
+    echo "$(echo_warn)WARNING: nodemon command not found, installing globally. $(echo_if 0)$(echo_clear)"
     run "sudo npm install -g nodemon" &
     NODEMON_PID=$!
     wait $NODEMON_PID
+    echo "$(echo_effect)Nodemon installed globally $(echo_if 1)$(echo_clear)"
 else
-    echo "nodemon found $(echo_if 1)"
+    echo "$(echo_effect)Nodemon found $(echo_if 1)$(echo_clear)"
 fi
 
 # Install Karma globally if not already installed.
+echo "$(echo_cause)Checking for Karma testing CLI$(echo_clear)"
 if [ ! -x "$(command -v karma)" ]; then
-    echo "WARNING: karma-cli command not found, installing globally. $(echo_if 0)"
+    echo "$(echo_warn)WARNING: karma-cli command not found, installing globally. $(echo_if 0)$(echo_clear)"
     run "sudo npm install -g karma-cli" &
     KARMA_PID=$!
     wait $KARMA_PID
+    echo "$(echo_effect)Karma installed globally $(echo_if 1)$(echo_clear)"
 else
-    echo "karma-cli found $(echo_if 1)"
+    echo "$(echo_effect)karma-cli found $(echo_if 1)$(echo_clear)"
 fi
 
 # Install Typescript globally if not already installed.
+echo "$(echo_cause)Checking for Typescript$(echo_clear)"
 if [ ! -x "$(command -v tsc)" ]; then
-    echo "WARNING: tsc command not found, installing globally. $(echo_if 0)"
+    echo "$(echo_warn)WARNING: tsc command not found, installing globally. $(echo_if 0)$(echo_clear)"
     run "sudo npm install -g typescript" &
     TSC_PID=$!
     wait $TSC_PID
+    echo "$(echo_effect)TSD installed globally $(echo_if 1)$(echo_clear)"
 else
-    echo "tsc found $(echo_if 1)"
+    echo "$(echo_effect)tsc found $(echo_if 1)$(echo_clear)"
 fi
 
 # Install Typescript Definition Utility globally if not already installed.
+echo "$(echo_cause)Checking for Typescript Definitions CLI$(echo_clear)"
 if [ ! -x "$(command -v tsd)" ]; then
-    echo "WARNING: tsd command not found, installing globally. $(echo_if 0)"
+    echo "$(echo_warn)WARNING: tsd command not found, installing globally. $(echo_if 0)$(echo_clear)"
     run "sudo npm install -g tsd" &
     TSD_PID=$!
-
     wait $TSD_PID
+    echo "$(echo_effect)Typescript installed globally $(echo_if 1)$(echo_clear)"
 else
-    echo "tsd found $(echo_if 1)"
+    echo "$(echo_effect)tsd found $(echo_if 1)$(echo_clear)"
 fi
 
 # Install NCU Definition Utility globally if not already installed.
+echo "$(echo_cause)Checking for NCU package utility for NPM$(echo_clear)"
 if [ ! -x "$(command -v ncu)" ]; then
-    echo "WARNING: ncu command not found, installing globally. $(echo_if 0)"
+    echo "$(echo_warn)WARNING: ncu command not found, installing globally. $(echo_if 0)$(echo_clear)"
     run "sudo npm install -g npm-check-updates" &
     NCU_PID=$!
-
     wait $NCU_PID
+    echo "$(echo_effect)NCU installed globally $(echo_if 1)$(echo_clear)"
 else
-    echo "ncu found $(echo_if 1)"
+    echo "$(echo_effect)ncu found $(echo_if 1)$(echo_clear)"
 fi
 
+
+
+#########################################################################################
+# NPM Core install
+echo "$(echo_cause)Starting core NPM install and setup$(echo_clear)"
+run 'npm config set registry http://registry.npmjs.org/'
+# NPM Clean dependencies
+run "npm prune"
+# NPM Install
 run "npm install"
-
+# NPM Link
 run "npm link"
+# NPM Complete
+echo "$(echo_effect)NPM module install complete $(echo_if 1)$(echo_clear)"
 
+
+#########################################################################################
+# Config Setup using Promptly in ./setup.js
+echo "$(echo_cause)Starting config setup$(echo_clear)"
+# start ./setup.js
 run "npm run setup-config" &
 SETUP_PID=$!
-
 wait $SETUP_PID
-echo "SUCCESS $(echo_if 1)$(echo_if 1)$(echo_if 1)$(echo_if 1): NPM Setup complete!"
+# config setup complete
+echo "$(echo_effect)Config setup complete $(echo_if 1)$(echo_clear)"
+
+#########################################################################################
+# Great Success!
+echo "$(echo_success)SUCCESS $(echo_if 1)$(echo_if 1)$(echo_if 1)$(echo_if 1): NPM Setup complete!$(echo_clear)"
+
+
