6ee64846e592b5804df7634b122312457abe7e32
Donovan Adams <donovan@hydrotik.com>
2015-12-23 18:43:12 -0500

    clean up formatting for setup.sh

--- src/global/server/views/home/Index.tsx
+++ src/global/server/views/home/Index.tsx
@@ -1,5 +1,39 @@
-// Default layout template
+/// <reference path="../../../typings/tsd.d.ts" />
 
+import * as React from 'react';
+
+interface IIndexProps {
+    title?: string;
+    js?: string;
+}
+
+class Index extends React.Component<IIndexProps, {}> {
+    public render(): React.ReactElement<Provider> {
+        return (
+            <html>
+                <head>
+
+                    <meta charSet="utf-8"></meta>
+                    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+                    <title>{this.props.title}</title>
+                    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"></link>
+                    <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'></link>
+                    </head>
+                <body style={{ fontFamily: ['Varela Round', 'sans-serif'] }}>
+                    <div id="app"></div>
+                    <script src={this.props.js}></script>
+                    </body>
+                </html>
+        );
+    }
+}
+
+
+
+
+
+// Default layout template
+/*
 var React = require('react');
 
 var Index = React.createClass({
@@ -26,3 +60,4 @@ var Index = React.createClass({
 });
 
 module.exports = Index;
+*/
