c1e3a7d9ec2b2858ca621e4e6904103825a9758a
Donovan Adams <donovan@hydrotik.com>
2016-01-11 15:31:35 -0500

    mongo sh start and stop working

--- script/helpers.sh
+++ script/helpers.sh
@@ -3,19 +3,6 @@
 set -e
 
 BUILD_DIR="build"
-COVERAGE_PORT="3001"
-COVERAGE_OUTPUT="$BUILD_DIR/reports/coverage"
-#JSDOC_PORT="3002"
-#JSDOC_OUTPUT="$BUILD_DIR/reports/jsdoc"
-#GITBOOK_PORT="3006"
-#GITBOOK_INPUT="doc"
-#GITBOOK_OUTPUT="$BUILD_DIR/reports/gitbook"
-#PLATO_CLONE_URL="git@github.com:saksdirect/saks.com.git"
-#PLATO_PORT="3003"
-#PLATO_OUTPUT="$BUILD_DIR/reports/plato"
-#STYLEGUIDE_PORT="3004"
-#STYLEGUIDE_OUTPUT="$BUILD_DIR/reports/styleguide"
-WEBPACK_PORT="3005"
 
 # https://coderwall.com/p/it3b-q/bash-include-guard
 if [ -n "$HELPERS_IMPORTED" ]; then return; fi
--- script/mongod.sh
+++ script/mongod.sh
@@ -10,52 +10,60 @@ source "$IMPORT_PATH/helpers.sh"
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #cd "${DIR}"
 
+WDIR="$( pwd )"
 mongod=/usr/local/bin/mongod
 mongod_data=/data/db
 mongod_log=/data/mongodb.log
 prog=mongod.sh
+PIDFILE=../mongod.pid
 RETVAL=0
 
 
 
-
-
 setup() {
-    if [ ! -d /data ] 
-    then
-        mkdir -p /data
-    fi
+    echo "Setting Up Mongo!"
+    # if [ ! -d /data ] 
+    # then
+    #     mkdir -p /data
+    # fi
 
-    if [ ! -d $mongod_data ] 
-    then
-        mkdir -p $mongod_data
-    fi
+    # if [ ! -d $mongod_data ] 
+    # then
+    #     mkdir -p $mongod_data
+    # fi
 }
 
 stop() {
-    grep_mongo=`ps aux | grep -v grep | grep "${mongod}"`
-    if [ ${#grep_mongo} -gt 0 ]
-    then
-    echo "Stop MongoDB."
-    PID=`ps x | grep -v grep | grep "${mongod}" | awk '{ print $1 }'`
-    `kill -2 ${PID}`
-    RETVAL=$?
-    else
-    echo "MongoDB is not running."
-    fi
+    echo "Stopping Mongo!"
+    SELPID=`cat $PIDFILE` && kill $SELPID
+    exit 0
+    # grep_mongo=`ps aux | grep -v grep | grep "${mongod}"`
+    # if [ ${#grep_mongo} -gt 0 ]
+    # then
+    # echo "Stop MongoDB."
+    # PID=`ps x | grep -v grep | grep "${mongod}" | awk '{ print $1 }'`
+    # `kill -2 ${PID}`
+    # RETVAL=$?
+    # else
+    # echo "MongoDB is not running."
+    # fi
 }
 start() {
-    grep_mongo=`ps aux | grep -v grep | grep "${mongod}"`
-    if [ -n "${grep_mongo}" ]
-    then
-    echo "MongoDB is already running."
-    else
-    echo "Start MongoDB."
-    mongod --dbpath ${mongod_data} &
-    TSD_PID=$!
-    wait $TSD_PID
-    RETVAL=$?
-    fi
+    echo "Starting Mongo!"
+    mongod --dbpath "${WDIR}${mongod_data}" &
+    echo $! > $PIDFILE
+    exit 0
+    # grep_mongo=`ps aux | grep -v grep | grep "${mongod}"`
+    # if [ -n "${grep_mongo}" ]
+    # then
+    # echo "MongoDB is already running."
+    # else
+    # echo "Start MongoDB."
+    # mongod --dbpath ${mongod_data} &
+    # TSD_PID=$!
+    # wait $TSD_PID
+    # RETVAL=$?
+    # fi
 }
 
 case "$1" in
