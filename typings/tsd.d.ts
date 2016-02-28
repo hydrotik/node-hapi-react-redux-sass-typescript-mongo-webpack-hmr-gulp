bcaede51b36057b161ec95690bdc0eccfcaae773
Donovan Adams <donovan@hydrotik.com>
2016-02-28 13:17:34 -0500

    linking to libs in json fetch

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -14,5 +14,6 @@
 /// <reference path="react-track/react-track-formulas.d.ts" />
 /// <reference path="react-imation/react-imation.d.ts" />
 /// <reference path="react-imation/tween-value-factories.d.ts" />
+/// <reference path="xhr/xhr.d.ts" />
 /// <reference path="qs/qs.d.ts" />
 /// <reference path="cookie/cookie.d.ts" />
