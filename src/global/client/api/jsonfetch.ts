b2d63b21e23ad18a32f7a157f9dabcaec7fac134
Donovan Adams <donovan@hydrotik.com>
2016-02-28 13:01:07 -0500

    install qs and cookie

--- src/global/client/api/jsonfetch.ts
+++ src/global/client/api/jsonfetch.ts
@@ -1,41 +1,58 @@
 /// <reference path="../../../../typings/tsd.d.ts" />
 
 import Xhr from 'xhr';
+import Cookie from 'cookie';
+import Qs from 'qs';
 
 export interface IJSONFetch {
     url: string;
     method: string;
     // Link to Data Interface
     data?: any;
+    query?: any;
 }
 
 export default function jsonFetch(options: IJSONFetch, callback: (error: Error, result?: string) => any): void {
 
-        let config: any = {
-            url: options.url,
-            method: options.method,
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        };
+    let cookies = Cookie.parse(document.cookie);
 
-        Xhr(config, function(err: Error, response: any, body: any): void {
+    let config: any = {
+        url: options.url,
+        method: options.method,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    };
 
-            if (err) {
-                callback(err);
-            } else if (response.statusCode >= 200 && response.statusCode < 300) {
-                if (response.headers.hasOwnProperty('x-auth-required')) {
-                    // RedirectActions.saveReturnUrl();
-                    window.location.href = '/login';
-                } else {
-                    callback(null, JSON.parse(body));
-                }
+    if (cookies.crumb) {
+        config.headers['X-CSRF-Token'] = cookies.crumb;
+    }
+
+    if (options.query) {
+        config.url += '?' + Qs.stringify(options.query);
+    }
+
+    if (options.data) {
+        config.body = JSON.stringify(options.data);
+    }
+
+    Xhr(config, function(err: Error, response: any, body: any): void {
+
+        if (err) {
+            callback(err);
+        } else if (response.statusCode >= 200 && response.statusCode < 300) {
+            if (response.headers.hasOwnProperty('x-auth-required')) {
+                // RedirectActions.saveReturnUrl();
+                window.location.href = '/login';
             } else {
-                let httpErr: Error = new Error(response.rawRequest.statusText);
-                callback(httpErr, JSON.parse(body));
+                callback(null, JSON.parse(body));
             }
-        });
+        } else {
+            let httpErr: Error = new Error(response.rawRequest.statusText);
+            callback(httpErr, JSON.parse(body));
+        }
+    });
 
     // let o: string = JSON.stringify(options.data);
 
