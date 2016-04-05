aae8f6d916f61c08f74a78c5791556adf66050d9
Donovan Adams <donovan@hydrotik.com>
2016-04-04 23:28:40 -0400

    started mapping

--- src/global/client/pages/dashboard/components/DashBoardVisitors/DashBoardVisitors.tsx
+++ src/global/client/pages/dashboard/components/DashBoardVisitors/DashBoardVisitors.tsx
@@ -2,14 +2,19 @@
 
 // Core Imports
 import * as React from 'react';
-// import { connect } from 'react-redux';
-import * as Moment from 'moment';
+import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines';
+import { MapGL } from 'react-map-gl';
+import * as Immutable from 'immutable';
+import { rasterTileStyle } from 'raster-tile-style';
 
 // Styles
 import './_DashBoardVisitors.scss';
 
+const MAPBOX_API:string = 'pk.eyJ1IjoiZGpkb25vdmFuIiwiYSI6ImNpbW1wa3FvYjAwNWt0cGx2ZWVidmdkcWkifQ.urcVfY5Pppwq2eWL97j5Sw';
+
 // Page Components
 
+// console.warn(MapGL);
 
 // Behaviors and Actions
 import {
@@ -34,6 +39,26 @@ export class DashBoardVisitors extends React.Component<IDashBoardVisitorsProps, 
 
     public render(): React.ReactElement<{}> {
 
+        // https://github.com/caspg/simple-data-table-map
+
+        const tileSource: string = '//tile.stamen.com/toner/{z}/{x}/{y}.png';
+
+        console.warn(rasterTileStyle);
+
+        const rts: any = rasterTileStyle([tileSource]);
+        console.warn(rts);
+        const mapStyle: any = Immutable.fromJS(rts);
+        const viewport: any = {
+          latitude: 37.78,
+          longitude: -122.45,
+          zoom: 11,
+          width: 800,
+          height: 800,
+          startDragLngLat: null,
+          isDragging: null
+        };
+
+
         return (
               <div className="box box-solid bg-light-blue-gradient">
                 <div className="box-header">
@@ -51,23 +76,43 @@ export class DashBoardVisitors extends React.Component<IDashBoardVisitorsProps, 
                   </h3>
                 </div>
                 <div className="box-body">
-                  <div id="world-map" style={{height: 250, width: '100%'}} />
+                  {/* <div id="world-map" style={{height: 250, width: '100%'}} /> */ }
+
+                  {/*
+                  https://mikewilliamson.wordpress.com/2016/02/24/using-mapbox-gl-and-webpack-together/
+                */}
+
+
+                  <MapGL
+                    mapStyle={mapStyle}
+                    {...viewport}
+                  />
+
                 </div>
                 {/* /.box-body*/}
                 <div className="box-footer no-border">
                   <div className="row">
                     <div className="col-xs-4 text-center" style={{borderRight: '1px solid #f4f4f4'}}>
-                      <div id="sparkline-1" />
+                      <Sparklines data={[1000, 1200, 920, 927, 931, 1027, 819, 930, 1021]} limit={10} width={80} height={50} margin={0}>
+                          <SparklinesLine style={{ stroke: "#92c1dc", fill: "#ebf4f9", fillOpacity: ".9" }} />
+                          <SparklinesSpots style={{ fill: "orange" }} />
+                      </Sparklines>
                       <div className="knob-label">Visitors</div>
                     </div>
                     {/* ./col */}
                     <div className="col-xs-4 text-center" style={{borderRight: '1px solid #f4f4f4'}}>
-                      <div id="sparkline-2" />
+                      <Sparklines data={[515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921]} limit={10} width={80} height={50} margin={0}>
+                          <SparklinesLine style={{ stroke: "#92c1dc", fill: "#ebf4f9", fillOpacity: ".9" }} />
+                          <SparklinesSpots style={{ fill: "orange" }} />
+                      </Sparklines>
                       <div className="knob-label">Online</div>
                     </div>
                     {/* ./col */}
                     <div className="col-xs-4 text-center">
-                      <div id="sparkline-3" />
+                      <Sparklines data={[15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21]} limit={10} width={80} height={50} margin={0}>
+                          <SparklinesLine style={{ stroke: "#92c1dc", fill: "#ebf4f9", fillOpacity: ".9" }} />
+                          <SparklinesSpots style={{ fill: "orange" }} />
+                      </Sparklines>
                       <div className="knob-label">Exists</div>
                     </div>
                     {/* ./col */}
--- webpack.config.client-hot.js
+++ webpack.config.client-hot.js
@@ -94,11 +94,6 @@ module.exports = {
             'webpack-dev-server/client?http://localhost:8080',
             'webpack/hot/only-dev-server',
             path.resolve(__dirname, './src/global/client/pages/login/index')
-        ],
-        dashboard: [
-            'webpack-dev-server/client?http://localhost:8080',
-            'webpack/hot/only-dev-server',
-            path.resolve(__dirname, './src/global/client/pages/dashboard/index')
         ]
         /* etc */
     },
