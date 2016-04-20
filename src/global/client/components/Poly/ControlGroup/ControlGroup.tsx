575a521476341bcec781778154759f2aa999dd35
David Hevron <david.o.hevron@gmail.com>
2016-04-20 00:04:03 -0400

    Test playground for Poly

---
+++ src/global/client/components/Poly/ControlGroup/ControlGroup.tsx
@@ -0,0 +1,77 @@
+/// <reference path='../../../../../../typings/main.d.ts' />
+
+// Core Imports
+import * as React from 'react';
+import * as ClassNames from 'classnames';
+import * as lodash from 'lodash';
+
+// Styles
+import './_ControlGroup.scss';
+
+// Interfaces
+interface IControlGroupProps {
+    hideLabel?: boolean;
+    hideHelp?: boolean;
+    children?: any;
+    hasError?: boolean;
+
+    groupClasses?: string;
+    labelClasses?: string;
+    helpClasses?: string;
+
+    label?: string;
+    help?: any;
+
+}
+interface IControlGroupState {
+}
+
+export class ControlGroup extends React.Component<IControlGroupProps, IControlGroupState> {
+
+    public constructor(props: any) {
+        super(props);
+    }
+
+    public render(): React.ReactElement<{}> {
+
+
+        let groupClasses: any = ClassNames(
+            lodash.assign(
+                {},
+                {
+                    'form-group': true,
+                    'has-error': this.props.hasError
+                },
+                this.props.groupClasses
+            ) as any
+        );
+
+        let labelClasses: any = ClassNames(lodash.assign({}, { 'control-label': true }, this.props.labelClasses) as any);
+
+        let helpClasses: any = ClassNames(lodash.assign({}, { 'help-block': true }, this.props.helpClasses) as any);
+
+        let labelComponent: React.ReactElement<{}>;
+
+        if (!this.props.hideLabel) {
+            labelComponent = <label className={labelClasses}>
+                {this.props.label}
+                </label>;
+        }
+
+        let helpComponent: React.ReactElement<{}>;
+
+        if (!this.props.hideHelp) {
+            helpComponent = <span className={helpClasses}>
+                {this.props.help}
+                </span>;
+        }
+
+        return (
+            <div className={groupClasses}>
+                {labelComponent}
+                {this.props.children}
+                {helpComponent}
+            </div>
+        );
+    }
+}
---
+++ src/global/client/components/Poly/TextControl/TextControl.tsx
@@ -0,0 +1,76 @@
+/// <reference path='../../../../../../typings/main.d.ts' />
+
+// Core Imports
+import * as React from 'react';
+import * as ClassNames from 'classnames';
+import * as lodash from 'lodash';
+
+// Styles
+import './_TextControl.scss';
+
+// Components
+import { ControlGroup } from '../ControlGroup/ControlGroup';
+
+// Interfaces
+interface ITextControlProps {
+    name: string; // = "name"
+    label: string; // = "Name"
+    ref?: string; // = "nameControl"
+    type?: string;
+    hasError?: boolean; // = { this.state.hasError.name }
+    valueLink?: any; // = { this.linkState('name') }
+    help?: string; // = { this.state.help.name }
+    disabled: boolean; // = { this.state.loading }
+
+    autoCapitalize?: string;
+    inputClasses?: any;
+    placeholder?: string;
+    value?: any;
+    onChange?: (func: any) => void;
+    onBlur?: (func: any) => void;
+
+}
+interface ITextControlState {
+}
+
+export class TextControl extends React.Component<ITextControlProps, ITextControlState> {
+
+    public constructor(props: any) {
+        super(props);
+    }
+
+    public render(): React.ReactElement<{}> {
+
+        let inputClasses: any = ClassNames(
+            lodash.assign(
+                {},
+                {
+                    'form-control': true
+                },
+                this.props.inputClasses
+            ) as any
+        );
+
+        return (
+            <ControlGroup
+                hasError={this.props.hasError}
+                label={this.props.label}
+                help={this.props.help}>
+
+                <input
+                    ref='inputField'
+                    type={this.props.type}
+                    autoCapitalize={this.props.autoCapitalize}
+                    className={inputClasses}
+                    name={this.props.name}
+                    placeholder={this.props.placeholder}
+                    value={this.props.value}
+                    disabled={this.props.disabled ? true : false}
+                    onChange={this.props.onChange}
+                    onBlur={this.props.onBlur}
+                    {...this.props}
+                    />
+                </ControlGroup>
+            );
+        }
+    }
