31cc44bfbfcdf046c19dd570da7100242c486619
ozguruksal <ozgur_uksal@s5a.com>
2017-06-22 16:28:20 -0400

    adds container for collection of plugins

---
+++ src/server/Manifest.js
@@ -0,0 +1,114 @@
+'use strict';
+
+const csi = require('./common');
+let env = process.env.NODE_ENV;
+
+let config = {
+    "server": {
+        "app": {
+            "slogan": "CSI-ONE"
+        }
+    },
+    "connections": [
+        {
+            "port": 8000,
+            "labels": ["web"]
+        },
+        {
+            "port": 8001,
+            "labels": ["admin"]
+        }
+    ],
+    "registrations": [
+
+// ============================================================================
+//
+// Third-party plugins should go here...
+// 
+// ============================================================================
+        {
+            "plugin": {
+                "register": "vision", // hapi vision plugin for rendering templates
+                "options": {}
+            }
+        },
+        {
+            "plugin": {
+                "register": "hapi-auth-jwt2", // Support for JWT
+                "options": {}
+            }
+        },
+        {
+            "plugin": {
+                "register": "hapi-auth-cookie", // Support for authentication/session via cookie
+                "options": {}
+            }
+        },
+        {
+            "plugin": {
+                "register": "crumb",
+                "options": {
+                    "skip": (request, reply) => {
+
+                        if (csi.get("route.settings.plugins.crumb.skip", request)) {
+                            return true;
+                        }
+
+                        return false;
+                    },
+                    "cookieOptions": {
+                        "isSecure": env != "development"
+                    } 
+                }
+            }
+            
+        },
+        {
+            "plugin": {
+                "register": "inert",
+                "options": {
+                }
+            }
+            
+        },
+        {
+            "plugin": {
+                "register": "h2o2",
+                "options": {}
+            }
+        },
+
+
+// ============================================================================
+//
+// Team-built plugins should go here...
+// 
+// ============================================================================
+        {
+            "plugin": {
+                "register": "../../node_modules/auth_plugin",
+                "options": {
+                    "ldapUrl": 'ldap://ldap.saksinc.com:389'
+                },
+                "select": ['admin'],
+                "routes": {
+                    "prefix": "/auth"
+                }
+            }
+        },
+
+       {
+            "plugin": {
+                "register": "../../node_modules/navbobulator",
+                "select": ["web"],
+                "routes": {
+                    "prefix": "/navbobulator"
+                },
+                "options": {
+                }
+            }
+        }
+    ]
+}
+
+module.exports = config;
---
+++ src/server/index.js
@@ -0,0 +1,199 @@
+'use strict';
+
+const csi = require('./common');
+const Path = require('path');
+const Hapi = require('hapi');
+
+// hapijs server and plugin composition
+const Glue = require('glue');
+
+// hapijs templates
+const Handlebars = require('handlebars');
+const Vision = require('vision');
+
+// hapijs authentication strategies
+const AuthCookie = require('hapi-auth-cookie');
+const AuthJWT = require('hapi-auth-jwt2');
+
+// hapijs logging, also using npm modules good-squeeze and good-console
+// https://github.com/hapijs/good-squeeze
+// https://github.com/hapijs/good-console
+const Good = require('good');
+
+const Inert = require('inert');
+
+// hapijs CSRF token generation
+const Crumb = require('crumb');
+
+// hapijs plugin for uncaught exceptions
+const Poop = require('poop');
+
+const Promise = require('bluebird');
+
+var MongoClient = require('mongodb').MongoClient
+
+//############################################################################
+//
+// Use Glue to compose plugins and configuration
+//
+// See hapi glue API for details on how to use pass config:
+// https://github.com/hapijs/glue/blob/master/API.md
+//
+//############################################################################
+const Manifest = require('./Manifest');
+
+const isDevMode = process.env.NODE_ENV === 'development';
+
+const glueOptions = {
+
+    // paths for plugins relative to the current directory
+    relativeTo: __dirname,
+    
+    // Register globally necessary plugins
+    preRegister: (server, next) => {
+        server.register([
+            {
+                "register": Good,
+                "options": {
+                    "ops": {
+                        "interval": 1000
+                    },
+                    "reporters": {
+                        "consoleReporter": [{
+                            "module": "good-squeeze",
+                            "name": "Squeeze",
+                            "args": [{ "error": "*", "log": "*", "response": "*" }]
+                        }, {
+                            "module": "good-console"
+                        }, "stdout"]
+                    }
+                }
+
+            }
+        ]
+        
+        , (err) => {
+            if (err) {
+                throw err;
+            }
+            server.log([], "Logging started");
+            if (process.env.MONGO_URI) {
+                MongoClient.connect(process.env.MONGO_URI, function(err, db) {
+                    if (!err) {
+                        server.log([], "Connected to mongo");
+                        server.app.wattsDb = db;
+                        let serverCollection = db.collection('header');
+                        serverCollection.find({}).toArray(function(err, docs) {
+                            if (err) {
+                                server.log([], err);
+                                db.close();
+                                return;
+                            }
+                        });
+                        
+                    }
+                    else {
+                        server.log([], err);
+                    }
+                });
+            }
+            else {
+                server.log(['error'], "No mongo defined");
+            }
+
+// ============================================================================
+//
+// Here, you can add a plugin without using Glue
+//
+//      server.register(
+//          { register: require('../../plugins/sample_plugin2') },
+//          { select: ['web'], routes: { prefix: '/sample2' } }, (err) => {
+//          if (err) {
+//              throw err;
+//          }
+//      });
+// ============================================================================
+
+            server.route({
+                method: 'GET',
+                path: '/',
+                handler: function (request, reply) {
+                    return reply('Hello Wrold!');
+                }
+            })
+
+            next();
+        });
+    }
+};
+
+Glue.compose(Manifest, glueOptions, (err, server) => {
+    if (err) {
+        throw err;
+    }
+
+    // Connection labels for admin and web are required!
+    let adminExists = false;
+    let webExists = false;
+    server.connections.forEach((curr, idx, arr) => {
+        if (curr.settings.labels.includes('admin') && !curr.settings.labels.includes('web')) {
+            adminExists = true;
+        }
+
+        if (curr.settings.labels.includes('web') && !curr.settings.labels.includes('admin')) {
+            webExists = true;
+        }
+    });
+
+    if (!adminExists || !webExists) {
+        throw new Error('Connection labels admin and web must exist and be on separate connections');
+    }
+
+    // Wrap hapi server.initialize and server.start in promises
+
+    function initialize() {
+        return new Promise((resolve, reject) => {
+            server.initialize((err) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                return resolve();
+            });
+        });
+    }
+
+    function start() {
+        return new Promise((resolve, reject) => {
+            server.on('request-error', function (request, err) {
+                console.log(err);
+                throw err;
+                //console.log('Error response (500) sent for request: ' + request.id + ' because: ' + (err.trace || err.stack || err));
+            });
+
+            server.start((err) => {
+                if (err) {
+                    return reject(err);
+                }
+                else {
+                    server.connections.map((curr, idx, arr) => {
+                        server.log(['debug'], curr.settings.labels + ' running at: ' + server.select(curr.settings.labels[0]).info.uri);
+                    });
+                    server.connections.map((curr, idx, arr) => {
+                        for (var plugin in curr.registrations) {
+                            let output = {
+                                name: curr.registrations[plugin].name,
+                                version: curr.registrations[plugin].version
+                            }
+                            server.log(['debug', curr.settings.labels[0]], 'Plugin registered on ' + curr.settings.labels + ' ' + output.name + '(' + output.version + ')');
+                        }
+                    });
+
+                    return resolve(server);
+                }
+            });
+        });
+    }
+
+    initialize().then(start);
+});
