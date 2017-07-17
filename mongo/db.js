89d8843d87a025a16aff7504df2fb64f4b9e3ef9
Hevron, David <David_Hevron@s5a.com>
2017-07-17 16:10:33 -0400

    Update db script

--- mongo/db.js
+++ mongo/db.js
@@ -2,5 +2,10 @@
 print("Collections found: " + db.getCollectionNames().length);
 
 db.createCollection('footers');
+db.createCollection('hello');
+db.createCollection('users');
+db.users.createIndex( { "username": 1 }, { unique: true } );
+db.hello.insert({name: 'watts'});
+
 
 print("Collections found: " + db.getCollectionNames().length);
