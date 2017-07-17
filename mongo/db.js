6432d60d2b5fe039a1d988e7366a0dea5f4196bf
Hevron, David <David_Hevron@s5a.com>
2017-07-17 16:30:13 -0400

    Update mongo docker script for local dev work

--- mongo/db.js
+++ mongo/db.js
@@ -3,8 +3,13 @@ print("Collections found: " + db.getCollectionNames().length);
 
 db.createCollection('footers');
 db.createCollection('hello');
-db.createCollection('users');
-db.users.createIndex( { "username": 1 }, { unique: true } );
+db.createCollection('user');
+db.user.createIndex( { "username": 1 }, { unique: true } );
+db.user.insert({
+    "username": "admin",
+    "password": "$2a$10$rQTAx8BFMlXvN.dezQ68duwvV1UsSiM31zFn.8HSOrazR21xqEAJm",
+    "salt": "$2a$10$rQTAx8BFMlXvN.dezQ68du"
+})
 db.hello.insert({name: 'watts'});
 
 
